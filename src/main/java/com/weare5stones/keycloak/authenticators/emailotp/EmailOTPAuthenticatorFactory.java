f816454b3b6cae6c02e36bbcfeefa3fea0c74a96
Jacob Spizziri <jspizziri@weare5stones.com>
2023-02-21 16:48:52 -0500

    style(config): change casing of simulation configuration language

--- src/main/java/com/weare5stones/keycloak/authenticators/emailotp/EmailOTPAuthenticatorFactory.java
+++ src/main/java/com/weare5stones/keycloak/authenticators/emailotp/EmailOTPAuthenticatorFactory.java
@@ -58,7 +58,7 @@ public class EmailOTPAuthenticatorFactory implements AuthenticatorFactory {
 			new ProviderConfigProperty("length", "Code length", "The number of digits of the generated code.", ProviderConfigProperty.STRING_TYPE, 6),
 			new ProviderConfigProperty("ttl", "Time-to-live", "The time to live in seconds for the code to be valid.", ProviderConfigProperty.STRING_TYPE, "300"),
 			new ProviderConfigProperty("senderId", "SenderId", "The sender ID is displayed as the message sender on the receiving device.", ProviderConfigProperty.STRING_TYPE, "Keycloak"),
-			new ProviderConfigProperty("simulation", "Simulation mode", "In simulation mode, the EMAIL won't be sent, but printed to the server logs", ProviderConfigProperty.BOOLEAN_TYPE, true)
+			new ProviderConfigProperty("simulation", "Simulation mode", "In simulation mode, the email won't be sent, but printed to the server logs", ProviderConfigProperty.BOOLEAN_TYPE, true)
 		);
 	}
 
