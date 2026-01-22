84b86744c9cf42d2f9ce47132cab09ca99f5b2e2
Jacob Spizziri <jspizziri@weare5stones.com>
2026-01-22 14:55:45 -0500

    feat: enhance OTP email functionality by adding "resend email" capability (#22)
    * Throttle OTP Email Sending: Implemented logic to ensure that the OTP email is sent only on the first render of the page, rather than on every refresh. This change will reduce the frequency of OTP emails and improve user clarity.
    
    * Add Resend OTP Feature: Introduced a button that allows users to request a resend of the OTP email if they have not received it. This provides users with a straightforward way to obtain a new OTP without needing to refresh the page.
    
    ---------
    
    Co-authored-by: touqeer.shafi <touqeer.shafi@gmail.com>
    Co-authored-by: Muhammad Touqeer <touqeer.shafi@stayinformed.de>

--- src/main/java/com/weare5stones/keycloak/authenticators/emailotp/EmailOTPAuthenticator.java
+++ src/main/java/com/weare5stones/keycloak/authenticators/emailotp/EmailOTPAuthenticator.java
@@ -6,17 +6,21 @@ import jakarta.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
 import org.jboss.logging.Logger;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.common.util.SecretGenerator;
+import org.keycloak.email.EmailException;
 import org.keycloak.email.EmailTemplateProvider;
 import org.keycloak.models.AuthenticatorConfigModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
 import org.keycloak.sessions.AuthenticationSessionModel;
+import org.keycloak.utils.StringUtil;
 
 public class EmailOTPAuthenticator implements Authenticator {
 
@@ -24,7 +28,9 @@ public class EmailOTPAuthenticator implements Authenticator {
   private static final String TOTP_EMAIL = "totp-email.ftl";
   private static final String AUTH_NOTE_CODE = "code";
   private static final String AUTH_NOTE_TTL = "ttl";
+  private static final String AUTH_NOTE_EMAIL_SENT = "emailSent";
   private static final String AUTH_NOTE_REMAINING_RETRIES = "remainingRetries";
+  private static final String AUTH_NOTE_REMAINING_RESEND_RETRIES = "remainingResendRetries";
   private static final Logger logger = Logger.getLogger(EmailOTPAuthenticator.class);
 
   private static final String ALPHA_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -33,52 +39,14 @@ public class EmailOTPAuthenticator implements Authenticator {
 
   @Override
   public void authenticate(AuthenticationFlowContext context) {
-    AuthenticatorConfigModel config = context.getAuthenticatorConfig();
-    KeycloakSession session = context.getSession();
-    UserModel user = context.getUser();
-
-    int ttl = Integer.parseInt(config.getConfig().get(EmailOTPAuthenticatorFactory.CONFIG_PROP_TTL));
-    String emailSubject = config.getConfig().get(EmailOTPAuthenticatorFactory.CONFIG_PROP_EMAIL_SUBJECT);
-    Boolean isSimulation = Boolean.parseBoolean(
-        config.getConfig()
-            .getOrDefault(
-                EmailOTPAuthenticatorFactory.CONFIG_PROP_SIMULATION,
-                "false"));
-
-    String code = getCode(config);
-    int maxRetries = getMaxRetries(config);
-    AuthenticationSessionModel authSession = context.getAuthenticationSession();
-    authSession.setAuthNote(AUTH_NOTE_CODE, code);
-    authSession.setAuthNote(AUTH_NOTE_TTL, Long.toString(System.currentTimeMillis() + (ttl * 1000L)));
-    authSession.setAuthNote(AUTH_NOTE_REMAINING_RETRIES, Integer.toString(maxRetries));
-
     try {
-      RealmModel realm = context.getRealm();
-
-      if (isSimulation) {
-        logger.warn(String.format(
-            "***** SIMULATION MODE ***** Would send a TOTP email to %s with code: %s",
-            user.getEmail(),
-            code));
-      } else {
-        String realmName = Strings.isNullOrEmpty(realm.getDisplayName()) ? realm.getName() : realm.getDisplayName();
-        List<Object> subjAttr = ImmutableList.of(realmName);
-        Map<String, Object> attributes = new HashMap<>();
-        attributes.put("code", code);
-        attributes.put("ttl", Math.floorDiv(ttl, 60));
-        session.getProvider(EmailTemplateProvider.class)
-            .setAuthenticationSession(authSession)
-            .setRealm(realm)
-            .setUser(user)
-            .setAttribute("realmName", realmName)
-            .send(
-                emailSubject,
-                subjAttr,
-                TOTP_EMAIL,
-                attributes);
+      boolean isSent = Boolean.parseBoolean(Optional.ofNullable(context.getAuthenticationSession().getAuthNote(AUTH_NOTE_EMAIL_SENT)).orElse("false"));
+      if(!isSent) {
+        sendEmailForOTP(context, false);
+        context.getAuthenticationSession().setAuthNote(AUTH_NOTE_EMAIL_SENT, "true");
       }
-
-      context.challenge(context.form().setAttribute("realm", context.getRealm()).createForm(TOTP_FORM));
+      context.challenge(context.form().setAttribute("realm", context.getRealm())
+        .createForm(TOTP_FORM));
     } catch (Exception e) {
       logger.error("An error occurred when attempting to email an TOTP auth:", e);
       context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR,
@@ -89,9 +57,44 @@ public class EmailOTPAuthenticator implements Authenticator {
 
   @Override
   public void action(AuthenticationFlowContext context) {
-    String enteredCode = context.getHttpRequest().getDecodedFormParameters().getFirst("code");
 
     AuthenticationSessionModel authSession = context.getAuthenticationSession();
+    AuthenticatorConfigModel config = context.getAuthenticatorConfig();
+
+    String resendEmail = context.getHttpRequest().getDecodedFormParameters().getFirst("resend");
+    if(!Strings.isNullOrEmpty(resendEmail) && "resend".equals(resendEmail)) {
+      try {
+
+        String remainingResendRetiresStr = authSession.getAuthNote(AUTH_NOTE_REMAINING_RESEND_RETRIES);
+        int remainingResendAttempts = remainingResendRetiresStr == null ? getMaxResendRetries(config) + 1 : Integer.parseInt(remainingResendRetiresStr);
+
+        if(remainingResendAttempts <= 0) {
+          context.failureChallenge(
+            AuthenticationFlowError.INVALID_CREDENTIALS,
+            context.form()
+              .setAttribute("realm", context.getRealm())
+              .setError("emailTOTPResendRetriesLimitReached")
+              .createForm(TOTP_FORM)
+          );
+          return;
+        }
+
+        sendEmailForOTP(context, true);
+        authSession.setAuthNote(AUTH_NOTE_REMAINING_RESEND_RETRIES, Integer.toString(remainingResendAttempts-1));
+        context.challenge(context.form().setAttribute("realm", context.getRealm())
+            .setInfo("otpMailSentAgain")
+          .createForm(TOTP_FORM));
+      } catch (Exception e) {
+        logger.error("An error occurred when attempting to email an TOTP auth:", e);
+        context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR,
+          context.form().setError("emailTOTPEmailNotSent", e.getMessage())
+            .createErrorPage(Response.Status.INTERNAL_SERVER_ERROR));
+      }
+      return;
+    }
+
+    String enteredCode = context.getHttpRequest().getDecodedFormParameters().getFirst("code");
+
     String code = authSession.getAuthNote(AUTH_NOTE_CODE);
     String ttl = authSession.getAuthNote(AUTH_NOTE_TTL);
     String remainingAttemptsStr = authSession.getAuthNote(AUTH_NOTE_REMAINING_RETRIES);
@@ -161,6 +164,15 @@ public class EmailOTPAuthenticator implements Authenticator {
     return maxRetries;
   }
 
+  private int getMaxResendRetries(AuthenticatorConfigModel config) {
+    int maxResendRetries = Integer.parseInt(
+      config.getConfig().getOrDefault(
+        EmailOTPAuthenticatorFactory.CONFIG_PROP_MAX_RETRIES, "3"
+      )
+    );
+    return maxResendRetries;
+  }
+
   private String getCode(AuthenticatorConfigModel config) {
     int length = Integer.parseInt(config.getConfig().get(EmailOTPAuthenticatorFactory.CONFIG_PROP_LENGTH));
     Boolean allowUppercase = Boolean.parseBoolean(
@@ -204,4 +216,54 @@ public class EmailOTPAuthenticator implements Authenticator {
         .randomString(length, symbols);
   }
 
+  private void sendEmailForOTP(AuthenticationFlowContext context, Boolean shouldUseExistingCode) throws EmailException {
+    AuthenticatorConfigModel config = context.getAuthenticatorConfig();
+    KeycloakSession session = context.getSession();
+    UserModel user = context.getUser();
+
+    int ttl = Integer.parseInt(config.getConfig().get(EmailOTPAuthenticatorFactory.CONFIG_PROP_TTL));
+    String emailSubject = config.getConfig().get(EmailOTPAuthenticatorFactory.CONFIG_PROP_EMAIL_SUBJECT);
+    AuthenticationSessionModel authSession = context.getAuthenticationSession();
+
+    Boolean isSimulation = Boolean.parseBoolean(
+      config.getConfig()
+        .getOrDefault(
+          EmailOTPAuthenticatorFactory.CONFIG_PROP_SIMULATION,
+          "false"));
+    String code = null;
+    if(shouldUseExistingCode) {
+      code = authSession.getAuthNote(AUTH_NOTE_CODE);
+    } else {
+      code = getCode(config);
+      authSession.setAuthNote(AUTH_NOTE_CODE, code);
+      int maxRetries = getMaxRetries(config);
+      authSession.setAuthNote(AUTH_NOTE_TTL, Long.toString(System.currentTimeMillis() + (ttl * 1000L)));
+      authSession.setAuthNote(AUTH_NOTE_REMAINING_RETRIES, Integer.toString(maxRetries));
+    }
+
+    RealmModel realm = context.getRealm();
+
+    if (isSimulation) {
+      logger.warn(String.format(
+        "***** SIMULATION MODE ***** Would send a TOTP email to %s with code: %s",
+        user.getEmail(),
+        code));
+    } else {
+      String realmName = Strings.isNullOrEmpty(realm.getDisplayName()) ? realm.getName() : realm.getDisplayName();
+      List<Object> subjAttr = ImmutableList.of(realmName);
+      Map<String, Object> attributes = new HashMap<>();
+      attributes.put("code", code);
+      attributes.put("ttl", Math.floorDiv(ttl, 60));
+      session.getProvider(EmailTemplateProvider.class)
+        .setAuthenticationSession(authSession)
+        .setRealm(realm)
+        .setUser(user)
+        .setAttribute("realmName", realmName)
+        .send(
+          emailSubject,
+          subjAttr,
+          TOTP_EMAIL,
+          attributes);
+    }
+  }
 }
